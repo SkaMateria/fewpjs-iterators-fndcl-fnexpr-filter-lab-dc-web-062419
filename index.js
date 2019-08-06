b8a61b96e07ecd720650c99e65213a7caaef5e04
SkaMateria <skamateria@gmail.com>
2019-08-06 19:30:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// const string = "SOBBY"
+
+function findMatching(drivers, string) {
+    let iMatch = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase()) 
+    return iMatch
+}
+
+function fuzzyMatch(drivers, string) {
+    let sMatch = drivers.filter(driver => driver.charAt(0) == string.charAt(0))
+    return sMatch
+}
+
+function matchName(drivers, string) {
+    let pMatch = drivers.filter(driver => driver.name == string)
+    return pMatch
+}
+
+
+
+
+
+
+
+// function findMatching(drivers, string) {
+//     let match = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+//     return match
+// }
+
+// function fuzzyMatch(drivers, string) {
+//     let match = drivers.filter(driver => driver.substring(0, string.length) === string) 
+//     return match 
+// }
+
+// function matchName(drivers, string) {
+//     let match = drivers.filter(function (el) {return el.name === string })
+//     return match 
+// }
